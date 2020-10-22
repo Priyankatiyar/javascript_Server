65bd186bd63be2956b4f0bbcc3212e61db213a3d
Priyankatiyar <priyanka.katiyar@successive.tech>
2020-10-22 14:47:21 +0530

    feature/39515

---
+++ extra/patterns/equilateral.js
@@ -0,0 +1,26 @@
+//let arguments = process.argv
+function formequilateral(n) {
+    pattern = "";
+    let space = n - 1;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j <space; j++) {
+            pattern += ' ';
+        }
+
+        for (let j = 0; j <= i; j++) {
+            pattern += '* ';
+            
+        }
+        
+
+        pattern += '\n';
+        space--;
+    }
+        console.log(pattern);
+    
+
+    
+}
+
+formequilateral(3);
