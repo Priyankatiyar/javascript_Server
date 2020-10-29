936093047cb5a7ae17305550a38d41ae7a2eb881
Priyankatiyar <priyanka.katiyar@successive.tech>
2020-10-29 10:04:48 +0530

    Creation of middleware and errorhandler

--- src/Server.ts
+++ src/Server.ts
@@ -1,4 +1,7 @@
 import * as express from 'express';
+import * as bodyparser from 'body-parser';
+import notFoundRoute from './libs/routes/notFoundRoute';
+import errorHandler from './libs/routes/errorHandler';
 class Server {
     private app;
     constructor(private config) {
@@ -6,16 +9,21 @@ class Server {
     }
     bootstrap() {
         this.setupRoutes();
+        this.initBodyParser();
         console.log('bootstrap');
         return this;
     }
     setupRoutes() {
-        const { PORT } = this.config;
-        console.log('PORT', PORT);
-        this.app.get('/health-check', (req, res, next) => {
+
+        this.app.use('/health-check', (req, res) => {
+            console.log('Inside Second middleware');
             res.send('I am OK');
         });
-        return this;
+        this.app.use(notFoundRoute);
+        this.app.use(errorHandler);
+    }
+    initBodyParser() {
+        this.app.use(bodyparser.json({ type: 'application/*+json' }));
     }
     run() {
         const{ app, config: { PORT } } = this;
