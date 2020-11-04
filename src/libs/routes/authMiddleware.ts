d10c491afbbe323d6334465f783b137f6362ac26
Priyankatiyar <priyanka.katiyar@successive.tech>
2020-11-04 22:27:15 +0530

    Updated validationHandler and validation

--- src/libs/routes/authMiddleware.ts
+++ src/libs/routes/authMiddleware.ts
@@ -1,14 +1,15 @@
 // import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+const jwt = require('jsonwebtoken');
 export default (module, permissionType) => (req, res, next) => {
     try {
     const { headers : { authorization: token }} = req;
-    console.log('token == ', token);
-    const decoder = jwt.verify( token, 'qwertyuiopasdfghjklzxcvbnm123456');
-    console.log( 'decoder', decoder);
+    // console.log('token == ', token);
+    const decoder = jwt.verify( token, 'ZzuPDhFPDFrJW1XtTJ7wY6cybmiheXpn');
+    // console.log( 'decoder', decoder);
     next();
     }
     catch (err) {
+    console.log('err =========== ', err);
         next( {
             error: 'Unauthorized',
             code: 403
